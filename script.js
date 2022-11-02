e18b16f32a34280189e54f4ee1a4b473d7e67369
Yasir-Mohi-ud-din <parrayyasir6@gamil.com>
2022-11-02 11:40:12 +0530

    done

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-let s = "abcba";
+
+
+
+// const s = "hello";
+// let n = s.length;
+// console.log(n);
+// let f =1;
+// for(let i =0,j=n-1;i<j;i++,j--){
+  
+//         if(s[i]===s[j])continue;
+//         else{
+//             f=0;
+//             break;
+     
+//     }
+
+    
+// }
+// if(f===1)
+// console.log("Yes");
+// else
+// console.log("No")
+const prompt = require('prompt-sync')();
+
+// console.log(process.argv);
+// const s = process.argv[2];
+console.log("Enter string");
+const s = prompt("");
+
 let n = s.length;
 console.log(n);
 let f =1;
@@ -14,6 +42,6 @@ for(let i =0,j=n-1;i<j;i++,j--){
     
 }
 if(f===1)
-console.log("Yes");
+console.log(`Yes, ${s} is  a palindrome`);
 else
-console.log("No")
+console.log(`No, ${s} is  not  a palindrome`)
